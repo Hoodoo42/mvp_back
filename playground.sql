213b0071fbcd9858cac0c394e4491788a678886e
Hoodoo42 <create@hoodooverse.ca>
2022-11-01 21:01:06 -0600

    created my basic tables and a few procedures in db, and started base for the python repo and dumped the db here.

---
+++ playground.sql
@@ -0,0 +1,209 @@
+-- MariaDB dump 10.19  Distrib 10.8.3-MariaDB, for osx10.17 (arm64)
+--
+-- Host: localhost    Database: Playground
+-- ------------------------------------------------------
+-- Server version	10.8.3-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `gamer`
+--
+
+DROP TABLE IF EXISTS `gamer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gamer` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) COLLATE utf8mb4_bin NOT NULL,
+  `password` varchar(150) COLLATE utf8mb4_bin NOT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Gamer_UNusername` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gamer`
+--
+
+LOCK TABLES `gamer` WRITE;
+/*!40000 ALTER TABLE `gamer` DISABLE KEYS */;
+INSERT INTO `gamer` VALUES
+(1,'user1','*22A99BA288DB55E8E230679259740873101CD636','2022-11-01'),
+(2,'user2','*1B884EF334D678B0743C8CE0425F980669D8FBB6','2022-11-01'),
+(3,'user3','*35B5E90BC4F5AE5D02ED515DF6B61141F24EDA02','2022-11-01'),
+(4,'user4','*B718D8767BBACFBCE02BA2669ECCEB05930FF7D7','2022-11-01');
+/*!40000 ALTER TABLE `gamer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gamer_session`
+--
+
+DROP TABLE IF EXISTS `gamer_session`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gamer_session` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `gamer_id` int(10) unsigned DEFAULT NULL,
+  `token` varchar(150) COLLATE utf8mb4_bin NOT NULL DEFAULT '1029384756',
+  `created_at` date NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `Gamer_Session_FK` (`gamer_id`),
+  CONSTRAINT `Gamer_Session_FK` FOREIGN KEY (`gamer_id`) REFERENCES `gamer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gamer_session`
+--
+
+LOCK TABLES `gamer_session` WRITE;
+/*!40000 ALTER TABLE `gamer_session` DISABLE KEYS */;
+INSERT INTO `gamer_session` VALUES
+(2,1,'213984729874','2022-11-01');
+/*!40000 ALTER TABLE `gamer_session` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `points`
+--
+
+DROP TABLE IF EXISTS `points`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `points` (
+  `points` int(10) unsigned DEFAULT 0,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `gamer_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `gamer_id_FK` (`gamer_id`),
+  CONSTRAINT `gamer_id_FK` FOREIGN KEY (`gamer_id`) REFERENCES `gamer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `points`
+--
+
+LOCK TABLES `points` WRITE;
+/*!40000 ALTER TABLE `points` DISABLE KEYS */;
+INSERT INTO `points` VALUES
+(12,1,1);
+/*!40000 ALTER TABLE `points` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'Playground'
+--
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `gamer_get` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `gamer_get`(id_input int unsigned)
+BEGIN
+	SELECT g.username, p.points 
+	FROM gamer g INNER JOIN points p ON p.gamer_id = g.id
+	WHERE g.id = id_input;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `gamer_login` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `gamer_login`(username_input varchar(20), password_input varchar(150), token_input varchar(100))
+    MODIFIES SQL DATA
+BEGIN
+	INSERT INTO gamer_session (token, gamer_id)
+	SELECT token_input, id FROM gamer WHERE username = username_input AND password = PASSWORD(password_input);
+	SELECT token, gamer_id FROM gamer_session gs WHERE token = token_input;
+	COMMIT;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `gamer_logout` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `gamer_logout`(token_input varchar(100))
+    MODIFIES SQL DATA
+BEGIN
+	DELETE FROM gamer_session WHERE token = token_input;
+	SELECT ROW_COUNT();
+	COMMIT;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `gamer_signup` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `gamer_signup`(username_input varchar(20), password_input varchar(150), token_input varchar(100))
+    MODIFIES SQL DATA
+BEGIN
+	
+	INSERT INTO gamer (username, password) VALUES (username_input, PASSWORD(password_input));
+	INSERT INTO gamer_session (token, gamer_id) SELECT token_input, id FROM gamer WHERE username = username_input;
+	SELECT ROW_COUNT();
+	COMMIT;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-01 20:59:04
---
+++ queries.sql
@@ -0,0 +1,4 @@
+CALL gamer_signup('user4', 'pass4', '42389479237');
+CALL gamer_login('user1', 'pass1', '213984729874');
+CALL gamer_get(4);
+CALL gamer_logout('42389479237');
