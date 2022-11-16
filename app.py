78f3a61f34bb2d208e887b34c5ee7ff3dac429f2
Hoodoo42 <create@hoodooverse.ca>
2022-11-15 22:58:26 -0700

    cleaning up for deploy

--- app.py
+++ app.py
@@ -10,39 +10,46 @@ app = Flask(__name__)
 CORS(app)
 
 # ## GAMER
+
+
 @app.get('/api/gamer')
 def get_gamer():
     is_valid = apih.check_endpoint_info(request.headers, ['id'])
-    if(is_valid != None):
+    if (is_valid != None):
         return make_response(json.dumps(is_valid, default=str), 400)
 
-    results = dbh.run_statement('CALL get_gamer(?)', [request.headers.get('id')])
-    if(type(results) == list):
+    results = dbh.run_statement(
+        'CALL get_gamer(?)', [request.headers.get('id')])
+    if (type(results) == list):
         return make_response(json.dumps(results, default=str), 200)
     else:
-        return make_response(json.dumps('Sorry Error', default=str), 500)     
+        return make_response(json.dumps('Sorry Error', default=str), 500)
+
 
 @app.post('/api/gamer')
 def gamer_signup():
     is_valid = apih.check_endpoint_info(request.json, ['username', 'password'])
-    if(is_valid != None):
+    if (is_valid != None):
         return make_response(json.dumps(is_valid, default=str), 400)
 
     token = uuid4().hex
-    results = dbh.run_statement('CALL gamer_signup(?,?,?)', [request.json.get('username'), request.json.get('password'), token])
-    if(type(results) == list):
+    results = dbh.run_statement('CALL gamer_signup(?,?,?)', [
+                                request.json.get('username'), request.json.get('password'), token])
+    if (type(results) == list):
         return make_response(json.dumps(results, default=str), 200)
     else:
-        return make_response(json.dumps('Sorry error', default=str), 500)  
+        return make_response(json.dumps('Sorry error', default=str), 500)
+
 
 @app.delete('/api/gamer')
 def delete_gamer():
     is_valid = apih.check_endpoint_info(request.json, ['password'])
-    if(is_valid != None):
+    if (is_valid != None):
         return make_response(json.dumps(is_valid, default=str), 400)
 
-    results = dbh.run_statement('CALL gamer_delete(?)', [request.json.get('password')])
-    if(type(results) == list):
+    results = dbh.run_statement('CALL gamer_delete(?)', [
+                                request.json.get('password')])
+    if (type(results) == list):
         return make_response(json.dumps(results, default=str), 200)
     else:
         return make_response(json.dumps('Sorry error', default=str), 500)
@@ -56,56 +63,64 @@ def delete_gamer():
 @app.post('/api/gamer_login')
 def login_gamer():
     is_valid = apih.check_endpoint_info(request.json, ['username', 'password'])
-    if(is_valid != None):
+    if (is_valid != None):
         return make_response(json.dumps(is_valid, default=str), 400)
 
     token = uuid4().hex
-    results = dbh.run_statement('CALL gamer_login(?,?,?)', [request.json.get('username'), request.json.get('password'), token])
-    if(type(results) == list):
+    results = dbh.run_statement('CALL gamer_login(?,?,?)', [
+                                request.json.get('username'), request.json.get('password'), token])
+    if (type(results) == list):
         return make_response(json.dumps(results, default=str), 200)
     else:
-        return make_response(json.dumps('sorry, error', default=str), 500) 
+        return make_response(json.dumps('sorry, error', default=str), 500)
+
 
 @app.delete('/api/gamer_login')
 def logout_gamer():
     is_valid = apih.check_endpoint_info(request.headers, ['token'])
-    if(is_valid != None):
+    if (is_valid != None):
         return make_response(json.dumps(is_valid, default=str), 400)
 
-    results = dbh.run_statement('CALL gamer_logout(?)', [request.headers.get('token')])
-    if(type(results) == list):
+    results = dbh.run_statement('CALL gamer_logout(?)', [
+                                request.headers.get('token')])
+    if (type(results) == list):
         return make_response(json.dumps(results, default=str), 200)
     else:
         return make_response(json.dumps('sorry, error', default=str), 500)
 
 # ## POINTS
 
+
 @app.get('/api/points')
 def get_all_points():
     results = dbh.run_statement('CALL points_get')
-    if(type(results) == list):
+    if (type(results) == list):
         return make_response(json.dumps(results, default=str), 200)
     else:
         return make_response(json.dumps('sorry, error', default=str), 500)
 
+
 @app.patch('/api/points')
 def update_points():
     is_valid = apih.check_endpoint_info(request.headers, ['gamer_id'])
-    if(is_valid != None):
+    if (is_valid != None):
         return make_response(json.dumps(is_valid, default=str), 400)
 
-    results = dbh.run_statement('CALL points_update(?,?)', [request.json.get('points_input'), request.headers.get('gamer_id')])
-    if(type(results) == list):
+    is_valid = apih.check_endpoint_info(request.json, ['points'])
+    if (is_valid != None):
+        return make_response(json.dumps(is_valid, default=str), 400)
+
+    results = dbh.run_statement('CALL points_update(?,?)', [
+                                request.json.get('points'), request.headers.get('gamer_id')])
+    if (type(results) == list):
         return make_response(json.dumps(results, default=str), 200)
     else:
-        return make_response(json.dumps('sorry, error', default=str), 500)   
+        return make_response(json.dumps('sorry, error', default=str), 500)
 
 
-
-        
 if (production_mode == True):
     print("Running in Production Mode")
-    importbjoern# type: ignore
+    import bjoern  # type: ignore
     bjoern.run(app, "0.0.0.0", 5042)
 else:
     from flask_cors import CORS
